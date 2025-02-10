47d1ac0980ab864f1263e91b65a2ea58614dc81c
EmirAyar <230101004@mgu.edu.tr>
2025-02-10 17:08:41 +0300

    Updates

--- js/parkur.js
+++ js/parkur.js
@@ -4,6 +4,7 @@
     const trackWidth = 30; // Her şerit genişliği
     const lanes = 6; // Şerit sayısı
     const innerRadius = 180; // İç çember yarıçapı
+    let posneg = true;
 
     const svg = d3
         .select("#parkur")
@@ -71,13 +72,13 @@
 
     // Çember üzerinde hareket
     const circleRadius = innerRadius + 2 * trackWidth + trackWidth / 2;
-    const speed = 0.001; // Hareket hızı
+    let speed = parseFloat(document.getElementById("speedSlider").value); // Hareket hızı
     let laps = 0; // Tur sayısını tutacak değişken
     let animation; // Animasyon zamanlayıcısını saklayacak değişken
     let previousAngle = 0; // Önceki açıyı takip et
     let stopLines = []; // Çizgileri saklamak için dizi
 
-    function drawStopLines(runnerX, runnerY, angle) {
+    function drawStopLines(runnerX, runnerY, angle, posneg) {
         // Orijine çizgiler ekle
         const line1 = svg
             .append("line")
@@ -109,47 +110,87 @@
             .duration(1000)
             .attr("x2", centerX + circleRadius)
             .attr("y2", centerY)
-            .on("end", () => {
-                let arcGenerator = d3
-                    .arc()
-                    .innerRadius(50)
-                    .outerRadius(45)
-                    .startAngle(0) // Başlangıç açısı 0
-                    .endAngle(0); // Animasyon başlarken 0 derece olacak
-
-                let arcPath = svg
-                    .append("path")
-                    .attr("class", "stop-arc")
-                    .attr(
-                        "transform",
-                        `translate(${centerX}, ${centerY}) rotate(90, 0, 0)`
-                    )
-                    .attr("fill", "red")
-                    .attr("d", arcGenerator()); // İlk olarak boş bir yay çiz
-
-                // **Animasyonu başlat**
-                arcPath
-                    .transition()
-                    .duration(1000) // 1 saniyede tamamlansın
-                    .attrTween("d", function () {
-                        let interpolate = d3.interpolate(0, -angle); // Açıyı sıfırdan angle'a getir
-                        return function (t) {
-                            return d3
-                                .arc()
-                                .innerRadius(50)
-                                .outerRadius(45)
-                                .startAngle(0)
-                                .endAngle(interpolate(t))(); // Açıyı kademeli artır
-                        };
-                    });
-            });
+            .on("end", () => drawStopArc(angle, posneg));
         // Çizgileri saklamak için diziye ekle
         stopLines.push(line1, line2);
     }
 
-    function startAnimation() {
+    function drawStopArc(angle, posneg) {
+        let arcGenerator = d3
+            .arc()
+            .innerRadius(50)
+            .outerRadius(45)
+            .startAngle(0)
+            .endAngle(0); // Başlangıç açısı 0
+
+        let arcPathEsas = svg
+            .append("path")
+            .attr("class", "stop-arc")
+            .attr(
+                "transform",
+                `translate(${centerX}, ${centerY}) rotate(90, 0, 0)`
+            )
+            .attr("fill", "red")
+            .attr("d", arcGenerator()); // İlk olarak boş bir yay çiz
+
+        // **Animasyonu başlat**
+        arcPathEsas
+            .transition()
+            .duration(2000) // 1 saniyede tamamlansın
+            .attrTween("d", function () {
+                let interpolate = d3.interpolate(0, -angle); // Açıyı pozitif veya negatif yönde artır
+                return function (t) {
+                    return d3
+                        .arc()
+                        .innerRadius(50)
+                        .outerRadius(45)
+                        .startAngle(0)
+                        .endAngle(interpolate(t))(); // Açıyı kademeli artır
+                };
+            });
+
+        if (posneg === false) {
+            const negAngleRad = (Math.ceil(360 - (angle * 180) / Math.PI) * Math.PI) / 180;
+            let arcPathGercek = svg
+                .append("path")
+                .attr("class", "stop-arc")
+                .attr(
+                    "transform",
+                    `translate(${centerX}, ${centerY}) rotate(90, 0, 0)`
+                )
+                .attr("fill", "blue")
+                .attr("d", arcGenerator()); // İlk olarak boş bir yay çiz
+
+            // **Animasyonu başlat**
+            arcPathGercek
+                .transition()
+                .duration(300) // 1 saniyede tamamlansın
+                .attrTween("d", function () {
+                    let interpolate = d3.interpolate(0, negAngleRad); // Açıyı pozitif veya negatif yönde artır
+                    return function (t) {
+                        return d3
+                            .arc()
+                            .innerRadius(50)
+                            .outerRadius(45)
+                            .startAngle(0)
+                            .endAngle(interpolate(t))(); // Açıyı kademeli artır
+                    };
+                });
+        }
+    }
+
+    function startAnimation(posneg) {
         if (!animation) {
-            // Çizgileri temizle
+            laps = 0;
+            document.getElementById(
+                "lapCounter"
+            ).textContent = `Turlar: ${laps}`;
+            // BUTON SLIDER DISABLED
+            document.getElementById("parkurPlay-btn").classList.add("disabled");
+            document.getElementById("speedSlider").classList.add("disabled");
+            document.getElementById("posnegCB").classList.add("disabled");
+            
+
 
             svg.selectAll(".stop-arc").remove(); // Önceki arc'ı sil
 
@@ -159,72 +200,137 @@
             runner.attr("xlink:href", "/images/runner.gif");
             animation = d3.timer((elapsed) => {
                 const angle = -(elapsed * speed) % (2 * Math.PI); // Açıyı hesapla
-                const x = centerX + circleRadius * Math.cos(angle); // X koordinatı
-                const y = centerY + circleRadius * Math.sin(angle); // Y koordinatı
+                const x =
+                    centerX + circleRadius * Math.cos(posneg ? angle : -angle); // X koordinatı
+                const y =
+                    centerY + circleRadius * Math.sin(posneg ? angle : -angle); // Y koordinatı
 
                 // Yönü kontrol et
                 const scaleX = y < centerY ? -1 : 1;
-
+                const yön = posneg ? scaleX : -scaleX;
                 runner
                     .attr("x", x - 35) // Koşan adamın X pozisyonu
                     .attr("y", y - 35) // Koşan adamın Y pozisyonu
                     .attr(
                         "transform",
-                        `translate(${x}, ${y}) scale(${scaleX}, 1) translate(${-x}, ${-y})`
+                        `translate(${x}, ${y}) scale(${yön}, 1) translate(${-x}, ${-y})`
                     );
 
-                // Başlangıç çizgisinden geçişi kontrol et
-                if (previousAngle < -Math.PI && angle >= -Math.PI) {
+                const degAngle = (-angle * 180) / Math.PI;
+
+                if (degAngle >= 355 && degAngle <= 360 && !lapsIncremented) {
                     laps += 1;
                     document.getElementById(
                         "lapCounter"
                     ).textContent = `Turlar: ${laps}`;
+                    lapsIncremented = true; // Lap artırıldığı için işaretle
+                }
+                if (degAngle < 355) {
+                    lapsIncremented = false;
                 }
                 // Açıyı güncelle
                 previousAngle = angle;
-
-                const newDegree = (-previousAngle * 180) / Math.PI + laps * 360;
-                document.getElementById(
-                    "degreeCounter"
-                ).textContent = `Derece: ${newDegree.toFixed(0)}`;
+                updateAngleDisplay(posneg);
             });
         }
     }
 
-    function stopAnimation() {
+    function stopAnimation(posneg) {
         if (animation) {
             animation.stop(); // Animasyonu durdur
             animation = null;
+
+            // BUTON SLIDER DISABLED
+            document.getElementById("parkurPlay-btn").classList.remove("disabled");
+            document.getElementById("speedSlider").classList.remove("disabled");
+            document.getElementById("posnegCB").classList.remove("disabled");
 
             // Çizgileri animasyonla ekle
             const runnerX = +runner.attr("x") + 35;
             const runnerY = +runner.attr("y") + 35;
             let angle = Math.atan2(runnerY - centerY, runnerX - centerX);
 
-            // Açıyı dereceye çevir
             let angleDeg = (angle * 180) / Math.PI;
 
             // Eğer açı negatifse, 360 ekleyerek pozitif yap
             if (angleDeg < 0) {
                 angleDeg += 360;
             }
-            console.log("angle", 360 - angleDeg);
-            angleRadSon = (angleDeg * Math.PI) / 180;
-            drawStopLines(runnerX, runnerY, 2 * Math.PI - angleRadSon);
 
-            laps = 0; // Turu sıfırla
-            document.getElementById(
-                "lapCounter"
-            ).textContent = `Turlar: ${laps}`;
+            angleRadSon = (angleDeg * Math.PI) / 180;
+            drawStopLines(runnerX, runnerY, 2 * Math.PI - angleRadSon, posneg);
+
             runner.attr("xlink:href", "/images/runner_still.png");
         }
+    }
+    
+
+    function updateAngleDisplay(posneg) {
+        const isDegree = document.getElementById("dereceCB").checked;
+        
+        // **Açı hesapla**
+        const allDegreePos = (-previousAngle * 180) / Math.PI + laps * 360;
+        const esasDegreePos = allDegreePos.toFixed(0) % 360;
+        const allDegreeNeg = (previousAngle * 180) / Math.PI - laps * 360;
+        const esasDegreeNeg = (allDegreeNeg.toFixed(0) % 360) + 360;
+        
+        // **Derece veya Radyanı Göster**
+        const displayValueRePo = isDegree
+        ? `${allDegreePos.toFixed(0)}°`
+        : `${(-previousAngle + laps * 2 * Math.PI).toFixed(2)} rad`;
+        
+        const displayValueEsPo = isDegree ? `${esasDegreePos}°` : `${(esasDegreePos * Math.PI / 180).toFixed(2)} rad`
+        
+        const displayValueReNeg = isDegree
+        ? `${allDegreeNeg.toFixed(0)}°`
+        : `${(allDegreeNeg.toFixed(0) * Math.PI / 180).toFixed(2)} rad`;
+        
+        const displayValueEsNeg = isDegree ? `${esasDegreeNeg}°` : `${(esasDegreeNeg * Math.PI / 180).toFixed(2)} rad`
+        
+        console.log("displayValueREPO",posneg)
+        
+        document.getElementById("degreeCounter").textContent = `Açı: ${posneg ? displayValueRePo : displayValueReNeg}`;
+        document.getElementById("esasCounter").textContent = `Esas ölçü: ${posneg ? displayValueEsPo : displayValueEsNeg}`;
     }
 
     // Başlat ve Durdur butonları
     document
         .getElementById("parkurPlay-btn")
-        .addEventListener("click", startAnimation);
+        .addEventListener("click", () => startAnimation(posneg));
     document
         .getElementById("parkurStop-btn")
-        .addEventListener("click", stopAnimation);
+        .addEventListener("click", () => stopAnimation(posneg));
+    document
+        .getElementById("speedSlider")
+        .addEventListener("input", function () {
+            speed = parseFloat(this.value); // Yeni hız değerini al
+        });
+
+    document.getElementById("dereceCB").addEventListener("change", function () {
+        if (this.checked) {
+            document.getElementById("radyanCB").checked = false;
+        } 
+        updateAngleDisplay(posneg);
+    });
+        
+    document.getElementById("radyanCB").addEventListener("change", function () {
+        if (this.checked) {
+            document.getElementById("dereceCB").checked = false;
+        }
+        updateAngleDisplay(posneg);
+    });
+
+    document.getElementById("pozitifCB").addEventListener("change", function () {
+        if (this.checked) {
+            posneg = true;
+            document.getElementById("negatifCB").checked = false;
+        }
+    });
+        
+    document.getElementById("negatifCB").addEventListener("change", function () {
+        if (this.checked) {
+            posneg = false;
+            document.getElementById("pozitifCB").checked = false;
+        }
+    });
 })();
