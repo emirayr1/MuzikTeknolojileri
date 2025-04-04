afc1177605d0a40353221824501378fdac988706
EmirAyar <230101004@mgu.edu.tr>
2025-04-05 01:07:31 +0300

    updates

---
+++ js/fizik/kuvvet/duvarİtme.js
@@ -0,0 +1,12 @@
+new p5((p) => {
+
+    p.setup = function () {
+        let canvas = p.createCanvas(700, 300);
+        canvas.parent("duvaritenadam");
+    };
+
+    p.draw = function () {
+        p.background(255);
+        
+    };
+});
